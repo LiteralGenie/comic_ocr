add text rotation and fix edge texts

src/lib/generate_text.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
+import math
 from pathlib import Path
 from random import randint
 import random
+from typing import Any
 from uuid import uuid4
 
 import cv2
@@ -10,7 +12,7 @@ from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 
 from lib.generate_bubbles import Bubble
-from lib.misc_utils import Bbox
+from lib.misc_utils import Bbox, rotate
 
 
 @dataclass
@@ -21,6 +23,7 @@ class Text:
     fp_font: Path
     font_size: float
     bbox: Bbox
+    angle: int
 
 
 def generate_texts(
@@ -30,6 +33,8 @@ def generate_texts(
     max_tries=2000,
     min_font_size=10,
     max_font_size=40,
+    min_angle=-30,
+    max_angle=30,
 ):
     mask = np.zeros((bubble.height, bubble.width, 3), np.uint8)
     mask.fill(255)
@@ -55,11 +60,20 @@ def generate_texts(
         font_size = randint(min_font_size, max_font_size)
         x = randint(0, bubble.width)
         y = randint(0, bubble.height)
+        angle = randint(min_angle, max_angle)
 
         render = Image.new("RGB", (mask.shape[1], mask.shape[0]))
+
         font = ImageFont.truetype(fp_font, font_size)
         draw = ImageDraw.Draw(render)
         draw.text((x + 1, y + 1), letter, font=font, fill=(255, 255, 255))
+        if _touches_edge(render):
+            continue
+
+        render = render.rotate(angle, fillcolor=(0, 0, 0))
+        if _touches_edge(render):
+            continue
+
         render = np.array(render)
 
         # check if letter extends outside bubble or intersects with existing letters
@@ -69,7 +83,8 @@ def generate_texts(
         if not is_valid:
             continue
 
-        mask = cv2.bitwise_or(render, mask)
+        _, tmp = cv2.threshold(render, 1, 255, cv2.THRESH_BINARY)
+        mask = cv2.bitwise_or(tmp, mask)
 
         try:
             bbox = _get_bbox(render)
@@ -77,7 +92,6 @@ def generate_texts(
             # render is empty or has 1-pixel in height / width
             continue
 
-        # print(letter, (x + bubble.bbox[1], y + bubble.bbox[0]), bubble.id, fp_font.stem)
         texts.append(
             Text(
                 uuid4().hex,
@@ -86,6 +100,7 @@ def generate_texts(
                 fp_font,
                 font_size,
                 bbox,
+                angle,
             )
         )
 
@@ -126,3 +141,23 @@ def _get_bbox(im: MatLike) -> Bbox:
             break
 
     return (y1, x1, y2, x2)
+
+
+def _touches_edge(im: Image.Image):
+    w, h = im.size
+
+    for x in range(w):
+        if im.getpixel((x, 0)) != (0, 0, 0):
+            return True
+
+        if im.getpixel((x, h - 1)) != (0, 0, 0):
+            return True
+
+    for y in range(h):
+        if im.getpixel((0, y)) != (0, 0, 0):
+            return True
+
+        if im.getpixel((w - 1, y)) != (0, 0, 0):
+            return True
+
+    return False

src/test.py
@@ -12,7 +12,7 @@ from lib.generate_text import Text, generate_texts
 
 FONT_DIR = Path(sys.argv[1])
 
-# random.seed(1)
+# random.seed(99)
 
 
 def main():
@@ -36,7 +36,7 @@ def main():
         for grp in bubble_map.values()
         for b in grp
     }
-    canvas = draw_texts(canvas, text_map)
+    canvas = draw_texts(canvas, bubble_map, text_map)
 
     cv2.imwrite("./tmp.png", canvas)
 
@@ -68,17 +68,38 @@ def draw_bubbles(canvas: MatLike, bubbles: dict[str, list[Bubble]]):
     return canvas
 
 
-def draw_texts(canvas: MatLike, texts: dict[str, list[Text]]):
-    pil_canvas = Image.fromarray(canvas)
+def draw_texts(
+    canvas: MatLike,
+    bubble_map: dict[str, list[Bubble]],
+    text_map: dict[str, list[Text]],
+):
+    pil_canvas = Image.fromarray(canvas).convert("RGBA")
 
-    for grp in texts.values():
+    for id_bubble, grp in text_map.items():
+        bubble = next(
+            b for grp in bubble_map.values() for b in grp if b.id == id_bubble
+        )
+        bubble_center = (
+            bubble.bbox[1] + bubble.width // 2,
+            bubble.bbox[0] + bubble.height // 2,
+        )
+
         for t in grp:
+            render = Image.new("RGBA", (canvas.shape[1], canvas.shape[0]), (0, 0, 0, 0))
             font = ImageFont.truetype(t.fp_font, t.font_size)
 
-            draw = ImageDraw.Draw(pil_canvas)
+            draw = ImageDraw.Draw(render)
             draw.text(t.xy, t.letter, font=font, fill=(0, 0, 255))
 
-    return np.array(pil_canvas)
+            render = render.rotate(
+                t.angle,
+                resample=Image.Resampling.BICUBIC,
+                center=bubble_center,
+            )
+
+            pil_canvas = Image.alpha_composite(pil_canvas, render)
+
+    return np.array(pil_canvas.convert("RGB"))
 
 
 main()